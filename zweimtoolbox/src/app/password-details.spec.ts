9d10fd136cd5efbe810a38edc4977b5b7f1f8326
Lisa Mack <lisa.m.tl@web.de>
2021-09-14 14:33:54 +0200

    added a password details object

---
+++ zweimtoolbox/src/app/password-details.spec.ts
@@ -0,0 +1,7 @@
+import { PasswordDetails } from './password-details';
+
+describe('PasswordDetails', () => {
+  it('should create an instance', () => {
+    expect(new PasswordDetails()).toBeTruthy();
+  });
+});
