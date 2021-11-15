ccf4db32302eae16fab6d187619ec7bf9d1fe37f
Lisa Mack <lisa.m.tl@web.de>
2021-11-15 09:47:03 +0100

    Add tests for password generation

--- zweimtoolbox/src/app/password-generator.spec.ts
+++ zweimtoolbox/src/app/password-generator.spec.ts
@@ -30,16 +30,25 @@ describe('PasswordGenerator', () => {
     expect(password.length).toBe(12);
   }); 
 
-  it('password should be easy', () => {
+  it('password should be a correct easy password', () => {
     let passwordDetails: PasswordDetails = new PasswordDetails(10, true, true);
     let password: string = generator.generatePassword(passwordDetails); 
     expect(password.length).toBe(10); 
     expect(password.substr(0, 1) == password.substr(0, 1).toUpperCase()).toBeTrue(); 
     expect(password.substr(1, 5) == password.substr(1, 5).toLocaleLowerCase() || password.substr(1, 4) == password.substr(1, 4).toLocaleLowerCase()).toBeTrue();
     expect(Number.isNaN(password.substr(5, 3)) && Number.isNaN(password.substr(6, 3))).toBeFalse();
-    // todo: test special signs
+    expect((!Number.isNaN(password.substr(8, 2)) && !password.substr(8, 2).match(/[a-zA-Z]/i)) || (!password.substr(8, 2).match(/[a-zA-Z]/i) && Number.isNaN(password.substr(9, 1)))).toBeTrue();
   });
 
-  // todo: test if generated passwords differ
+  it('generated passwords should differ', () => {
+    let passwordDetails: PasswordDetails = new PasswordDetails(15); 
+    let password1: string = generator.generatePassword(passwordDetails); 
+    let password2: string = generator.generatePassword(passwordDetails); 
+    let password3: string = generator.generatePassword(passwordDetails); 
+    expect(password1).not.toEqual(password2); 
+    expect(password2).not.toEqual(password3); 
+    expect(password3).not.toEqual(password1); 
+  })
+
 
 });
